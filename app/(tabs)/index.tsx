f31bb5daa283bc1d59aaebfbac7503c89c71ed5e
PalaashPandey <palaashpandey2006@gmail.com>
2025-02-15 14:58:02 -0600

    can tap of topbar to close keyboard

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View, Text, Pressable, FlatList } from "react-native";
+import { View, Text, Pressable, FlatList, Keyboard, TouchableWithoutFeedback } from "react-native";
 import EventCard from '../../components/EventCard';
 import { mockEvents } from '@/api/fakedb';
 import { tabBarHeight } from './_layout';
@@ -11,25 +11,27 @@ export default function ExplorePage() {
 
   return (
     <View className="flex-1 bg-gray-50">
-      <View className="flex-row justify-center bg-white border-b border-gray-200">
-        {(['for you', 'following'] as Tab[]).map((tab) => (
-          <Pressable
-            key={tab}
-            onPress={() => setActiveTab(tab)}
-            className={`px-6 py-4 ${
-              activeTab === tab ? 'border-b-2 border-maroon-600' : ''
-            }`}
-          >
-            <Text
-              className={`text-lg capitalize ${
-                activeTab === tab ? 'text-maroon-600 font-semibold' : 'text-gray-600'
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View className="flex-row justify-center bg-white border-b border-gray-200">
+          {(['for you', 'following'] as Tab[]).map((tab) => (
+            <Pressable
+              key={tab}
+              onPress={() => setActiveTab(tab)}
+              className={`px-6 py-4 ${
+                activeTab === tab ? 'border-b-2 border-maroon-600' : ''
               }`}
             >
-              {tab}
-            </Text>
-          </Pressable>
-        ))}
-      </View>
+              <Text
+                className={`text-lg capitalize ${
+                  activeTab === tab ? 'text-maroon-600 font-semibold' : 'text-gray-600'
+                }`}
+              >
+                {tab}
+              </Text>
+            </Pressable>
+          ))}
+        </View>
+      </TouchableWithoutFeedback>
 
       <FlatList 
         data = {mockEvents}
