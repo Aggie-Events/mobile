f14c30878aaa2c2408a911c96333dc6862a11526
Jadon Lee <jadonlee888@gmail.com>
2025-02-18 16:14:44 -0600

    added react native google signin package

--- config/api-url.ts
+++ config/api-url.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://10.119.77.107:5000/api";
+export const API_URL = "http://10.246.117.237:5000/api";
 // HOWTO:
 // 1. run ipconfig on your machine (windows) (not sure what the mac equivalent is)
 // 2. find the ipv4 address
